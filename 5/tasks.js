09e42abaff34e73fb794cb6a33fa3f6bb2b5f167
Мызников Евгений Игоревич <myznikovei@admlr.lipetsk.ru>
2022-11-09 12:53:09 +0300

    fix: 5 hw

--- 5/tasks.js
+++ 5/tasks.js
@@ -1,73 +1,23 @@
 let dead = document.getElementById('dead')
 let lost = document.getElementById('lost')
-hole1 = document.getElementById('hole1')
-hole2 = document.getElementById('hole2')
-hole3 = document.getElementById('hole3')
-hole4 = document.getElementById('hole4')
-hole5 = document.getElementById('hole5')
-hole6 = document.getElementById('hole6')
-hole7 = document.getElementById('hole7')
-hole8 = document.getElementById('hole8')
-hole9 = document.getElementById('hole9')
 
 
-hole1.onclick = () => {
-    if (hole1.classList.contains( 'hole_has-mole' ) === true) dead.textContent++
-    else lost.textContent++
-}
-hole2.onclick = () => {
-    if (hole2.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
-hole3.onclick = () => {
-    if (hole3.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
-hole4.onclick = () => {
-    if (hole4.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
-hole5.onclick = () => {
-    if (hole5.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
-hole6.onclick = () => {
-    if (hole6.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
-hole7.onclick = () => {
-    if (hole7.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
-hole8.onclick = () => {
-    if (hole8.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
-hole9.onclick = () => {
-    if (hole9.classList.contains( 'hole_has-mole' ) === true) {
-        dead.textContent++
-    }else{
-        lost.textContent++
-    }
-}
+let holes = document.querySelectorAll('.hole')
+holes.forEach(el => {
+    el.addEventListener('click', () => {
+        if (+lost.textContent === 5) {
+            alert('You lose')
+        }
+
+        if (+dead.textContent === 10) {
+            alert('You win')
+        }
+        
+        if (el.classList.contains( 'hole_has-mole' ) === true) {
+            dead.textContent++
+        }
+        else{
+            lost.textContent++
+        }
+    })
+})
