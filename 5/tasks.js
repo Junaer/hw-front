4627fadcf898d95c53b9d7bffc7e1c2bd93c4756
Мызников Евгений Игоревич <myznikovei@admlr.lipetsk.ru>
2022-11-14 11:53:57 +0300

    fix: fk js

--- 5/tasks.js
+++ 5/tasks.js
@@ -7,12 +7,14 @@ holes.forEach(el => {
     el.addEventListener('click', () => {
         if (+lost.textContent === 5) {
             alert('You lose')
-            location.reload()
+            lost.textContent = -1
+            dead.textContent = 0
         }
 
         if (+dead.textContent === 10) {
             alert('You win')
-            location.reload()
+            lost.textContent = 0
+            dead.textContent = -1
         }
         
         if (el.classList.contains( 'hole_has-mole' ) === true) {
