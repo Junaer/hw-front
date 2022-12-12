b5bfdb7407252fb8d762fa54f89e1cf511855610
Мызников Евгений Игоревич <myznikovei@admlr.lipetsk.ru>
2022-12-12 15:36:13 +0300

    fix: hw 6

--- 6/menu/task.js
+++ 6/menu/task.js
@@ -3,15 +3,17 @@ keybords.forEach(el => {
     el.addEventListener('click', function (event) {
             const parentel = el.closest('.menu__item')
             const menu_el=parentel.querySelector('.menu_sub')
-
-            if (menu_el) {
+            console.log(menu_el);
+            if (menu_el.className == 'menu menu_sub') {
                 const menu_act=document.querySelector('.menu_active')
                 if (menu_act) {
                     menu_act.classList.remove('menu_active')
                 }
-                console.log(menu_act)
                 menu_el.classList.add('menu_active')
                 event.preventDefault();
+            }else{
+                const menu_act=document.querySelector('.menu_active')
+                menu_act.classList.remove('menu_active')
             }
         })
     })
--- 6/slider/task.js
+++ 6/slider/task.js
@@ -1,25 +1,29 @@
 const back = document.getElementsByClassName('slider__arrow slider__arrow_prev')[0]
 const next = document.getElementsByClassName('slider__arrow slider__arrow_next')[0]
 const photo = document.querySelectorAll('.slider__items img')
-const main_photo = document.getElementsByClassName('slider__item_active')
-let count = 0
+const mainPhoto = document.getElementsByClassName('slider__item_active')
+
+
 
 back.addEventListener('click', function () {
-    if (count == 0) {
-        count = photo.length
+    const slider = document.querySelectorAll('.slider__item')
+    let arr = Array.from(slider);
+    let id = arr.findIndex(el => el.className=='slider__item slider__item_active');
+    if (id == 0) {
+        id = photo.length
     }
     document.getElementsByClassName('slider__item_active')[0].classList.remove('slider__item_active')
-    count--
-    photo[count].closest('.slider__item').classList.add('slider__item_active')
+    id--
+    photo[id].closest('.slider__item').classList.add('slider__item_active')
   })
 next.addEventListener('click', function () {
-    count++
-    
-    if (count == photo.length) {
-        count = 0
+    const slider = document.querySelectorAll('.slider__item')
+    let arr = Array.from(slider);
+    let id = arr.findIndex(el => el.className=='slider__item slider__item_active');
+    id++
+    if (id == photo.length) {
+        id = 0
     } 
- 
     document.getElementsByClassName('slider__item_active')[0].classList.remove('slider__item_active')
-    
-    photo[count].closest('.slider__item').classList.add('slider__item_active')
+    photo[id].closest('.slider__item').classList.add('slider__item_active')
 })
