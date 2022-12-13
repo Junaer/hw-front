5a968ceabf570f17d210bf4a84c2037a07f97403
Мызников Евгений Игоревич <myznikovei@admlr.lipetsk.ru>
2022-12-13 12:01:15 +0300

    fix: hw 6 =(

--- 6/menu/task.js
+++ 6/menu/task.js
@@ -2,18 +2,23 @@ let keybords = document.querySelectorAll('.menu__link')
 keybords.forEach(el => {
     el.addEventListener('click', function (event) {
             const parentel = el.closest('.menu__item')
-            const menu_el=parentel.querySelector('.menu_sub')
-            console.log(menu_el);
-            if (menu_el.className == 'menu menu_sub') {
-                const menu_act=document.querySelector('.menu_active')
-                if (menu_act) {
-                    menu_act.classList.remove('menu_active')
-                }
-                menu_el.classList.add('menu_active')
+            const menuEl=parentel.querySelector('.menu_sub')
+            console.log(menuEl);
+            if (menuEl.className == 'menu menu_sub' || menuEl.className == 'menu menu_sub menu_active') {
                 event.preventDefault();
-            }else{
-                const menu_act=document.querySelector('.menu_active')
-                menu_act.classList.remove('menu_active')
+                if (menuEl.className == "menu menu_sub menu_active") {
+                    menuEl.classList.remove('menu_active')
+                }else{
+                    menuEl.classList.add('menu_active')
+                }
+                
+                
             }
         })
     })
+
+
+// const menuEl=document.querySelectorAll('.menu__item')
+// console.log(menuEl[1].textContent);
+// console.log(menuEl[1].firstElementChild.href);
+// event.preventDefault();
--- 6/popups/task.js
+++ 6/popups/task.js
@@ -1,14 +1,17 @@
-const closekk = document.getElementById('closs');
+const closs = document.getElementById('closs');
+const closeSu = document.getElementById('')
 const close_success = document.getElementById("closs_success");
 const nice = document.getElementById('nice');
+let name = 'Evgen'
 
-closekk.onclick = () => {
-    document.getElementById("modal_main").classList.remove('modal_active')
-}
-close_success.onclick = () => {
-    document.getElementById("modal_success").classList.remove('modal_active')
-}
-nice.onclick = () => {
+
+closs.addEventListener('click', () => document.getElementById("modal_main").classList.remove('modal_active'))
+
+
+close_success.addEventListener('click', () => document.getElementById("modal_success").classList.remove('modal_active'))
+
+
+nice.addEventListener('click', () => {
     document.getElementById("modal_main").classList.remove('modal_active')
     document.getElementById("modal_success").classList.add('modal_active')
-}
+})
